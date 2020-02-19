cf69bc853c4a0ddfc350be4d9c80dbd3e5c427d1
nikniksham <nikniksham@gmail.com>
2020-02-19 15:08:43 +0300

    3 классная задача

--- site.py
+++ site.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def name():
     return '''<!DOCTYPE html>
@@ -14,6 +15,8 @@ def name():
        <h1>Миссия Колонизация Марса</h1>
    </body>
 </html>'''
+
+
 @app.route('/index/')
 def apple():
     return '''<!DOCTYPE html>
@@ -27,6 +30,8 @@ def apple():
        <h1>И на марсе будут яблони цвести!</h1>
    </body>
 </html>'''
+
+
 @app.route('/promotion/')
 def promotion():
     return """<!DOCTYPE html>
@@ -40,13 +45,21 @@ def promotion():
     <h1>Человечеству мала одна планета.</h1>
     <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
     <h1>И начнем с Марса!</h1>
-    <h1>Присоединяйся!</h1>
 </body>
 </html>"""
+
+
 @app.route('/image_mars/')
 def mars():
     return render_template('1.html', filename=url_for('static', filename='img/mars.jpg'))
 
 
+@app.route('/promotion_image/')
+def mars_promotion():
+    return render_template('2.html', style=url_for('static', filename='css/style.css'),
+                           filename=url_for('static', filename='img/mars.jpg'))
+
+
 if __name__ == '__main__':
+    print('http://127.0.0.1:8000/promotion_image/')
     app.run(port=8000, host='127.0.0.1')
